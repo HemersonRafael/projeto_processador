7ecb70131ecd8bca39152d989017ed0efc9de028
HemersonRafael <rafaelpontes1995@gmail.com>
2018-06-29 19:43:40 -0300

    update files

--- processador/cpu.vhdl
+++ processador/cpu.vhdl
@@ -8,140 +8,62 @@ use IEEE.std_logic_arith.all;
 
 entity cpu is
    port ( 
-			rst           : in  STD_LOGIC;
-			start         : in  STD_LOGIC;
-         clk           : in  STD_LOGIC;
-			output        : out STD_LOGIC_VECTOR(3 downto 0);
-       	a,b,c,d,e,f,g : out std_logic
-          
-        );
+		rst     : in  STD_LOGIC;
+		start   : in  STD_LOGIC;
+		clk     : in  STD_LOGIC;
+		output  : out STD_LOGIC_VECTOR(3 downto 0);
+		HEX0    : out STD_LOGIC_VECTOR(6 downto 0);
+		HEX1 	  : out STD_LOGIC_VECTOR(6 downto 0);
+		HEX2 	  : out STD_LOGIC_VECTOR(6 downto 0);
+		HEX3 	  : out STD_LOGIC_VECTOR(6 downto 0)
+   );
 end cpu;
 
 architecture struc of cpu is
 
-component ctrl 
-   port ( rst   : in STD_LOGIC;
-			 start : in STD_LOGIC;
-          clk   : in STD_LOGIC;
-          imm   : out std_logic_vector(3 downto 0)
-          
-        );
-end component;
+	component conversor_7seg
+		port(
+		  entrada 	: in  std_logic_vector (3 downto 0);  -- vetor de entrada com 4 bits
+		  segmentos : out std_logic_vector (6 downto 0)-- vetor de saida que vai receber o valor de entrada representando em 7 bits
+		);
+	end component;
+	
+	component ctrl 
+		port ( 
+			rst   : in STD_LOGIC;
+			start : in STD_LOGIC;
+			clk   : in STD_LOGIC;
+			imm   : out std_logic_vector(3 downto 0)
+		);
+	end component;
 
-component dp
-   port (
+	component dp
+		port (
 			rst     : in STD_LOGIC;
-         clk     : in STD_LOGIC;
+			clk     : in STD_LOGIC;
 			imm     : in std_logic_vector(3 downto 0);
-         output_4: out STD_LOGIC_VECTOR (3 downto 0)
-          
-        );
-end component;
+			output_4: out STD_LOGIC_VECTOR (3 downto 0)    
+		);
+	end component;
 
 
-signal immediate : std_logic_vector(3 downto 0);
-signal cpu_out : std_logic_vector(3 downto 0);
+	signal immediate : std_logic_vector(3 downto 0);
+	signal cpu_out : std_logic_vector(3 downto 0);
 
-begin
-
-
-
-  controller: ctrl port map(rst, start, clk,immediate);
-  datapath: dp port map(rst, clk, immediate, cpu_out);
-
-
-  process(rst, clk, cpu_out)
-  begin
-
-
-    if(clk'event and clk='1') then
-    output <= cpu_out;
-    
-       case cpu_out is
-         when "0000" =>
-           a <= '0'; 
-			  b <= '0'; 
-			  c <= '0'; 
-			  d <= '0'; 
-           e <= '0'; 
-			  f <= '0'; 
-			  g <= '1';
-         when "0001" =>
-           a <= '1'; 
-			  b <= '0'; 
-			  c <= '0'; 
-			  d <= '1'; 
-           e <= '1'; 
-			  f <= '1'; 
-			  g <= '1';
-         when "0010" =>
-			  a <= '0'; 
-			  b <= '0'; 
-			  c <= '1'; 
-			  d <= '0'; 
-           e <= '0'; 
-			  f <= '1'; 
-			  g <= '0';
-         when "0011" =>
-           a <= '0'; 
-			  b <= '0'; 
-			  c <= '0'; 
-			  d <= '0'; 
-           e <= '1'; 
-			  f <= '1'; 
-			  g <= '0';
-         when "0100" =>
-           a <= '1'; 
-			  b <= '0'; 
-			  c <= '0'; 
-			  d <= '1'; 
-           e <= '1'; 
-			  f <= '0'; 
-			  g <= '0';
-         when "0101" =>
-           a <= '0'; 
-			  b <= '1'; 
-			  c <= '0'; 
-			  d <= '0'; 
-           e <= '1'; 
-			  f <= '0'; 
-			  g <= '0';
-         when "0110" =>
-           a <= '0'; 
-			  b <= '1'; 
-			  c <= '0'; 
-			  d <= '0'; 
-           e <= '0'; 
-			  f <= '0'; 
-			  g <= '0';
-         when "0111" =>
-           a <= '0'; 
-			  b <= '0'; 
-			  c <= '0'; 
-			  d <= '1'; 
-           e <= '1'; 
-			  f <= '1'; 
-			  g <= '1';
-         when "1000" =>
-           a <= '0'; 
-			  b <= '0'; 
-			  c <= '0'; 
-			  d <= '0'; 
-           e <= '0'; 
-			  f <= '0'; 
-			  g <= '0';
-         when "1001" =>
-           a <= '0'; 
-			  b <= '0'; 
-			  c <= '0'; 
-			  d <= '0'; 
-           e <= '0'; 
-			  f <= '1'; 
-			  g <= '0';
-         when others =>
-       end case;
-    end if;
-  end process;							
+	begin
+	  
+	  controller: ctrl port map(rst, start, clk,immediate);
+	  datapath: dp port map(rst, clk, immediate, cpu_out);
+	  C7S: conversor_7seg port map(cpu_out,HEX);
+	  
+	  process(rst, clk, cpu_out)
+	  begin
+		  if(clk'event and clk='1') then
+			output <= cpu_out;
+		  end if;
+	  end process;
+	  
+	-- end begin
 
 end struc;
 
